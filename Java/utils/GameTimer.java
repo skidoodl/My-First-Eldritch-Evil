39525137abfaba7f04ad0a14cad7a691883f6501
Enilks <144824982+Enilks@users.noreply.github.com>
2023-12-12 09:46:18 -0500

    Update GameTimer.java
    Added JOptionPane alerts for timer errors

--- Java/utils/GameTimer.java
+++ Java/utils/GameTimer.java
@@ -1,4 +1,7 @@
 package utils;
+
+import javax.swing.JOptionPane;
+
 public class GameTimer {
     private long time,startTime;
     private boolean timerActive = false;
@@ -11,7 +14,8 @@ public class GameTimer {
         if (timerActive){
             System.out.println("   >>Could not start game timer because\n"+
                                "     it was already running.\n");
-            Lazy.waitForEnter(true);
+            //TODO - Transfer to seperate Alerts class (keeping this here for now though)
+            JOptionPane.showMessageDialog(null,"Could not start game timer because it was already running.","Game Timer Error",JOptionPane.ERROR_MESSAGE);
             return;
         }
         startTime = System.nanoTime();
@@ -22,6 +26,8 @@ public class GameTimer {
         if(!timerActive){
             System.out.println("   >>Could not pause game timer because\n"+
                                "     it was already paused.");
+            //TODO - Transfer to seperate Alerts class (keeping this here for now though)
+            JOptionPane.showMessageDialog(null,"Could not pause game timer because it was already paused.","Game Timer Error",JOptionPane.ERROR_MESSAGE);
             return;
         }
         timerActive = false;
